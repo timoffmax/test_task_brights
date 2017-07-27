d43ac437ee7b4e99a64918130cf9e334809c4e9e
Timofey Maksimenko <tim.off.max@gmail.com>
2017-07-27 22:32:52 +0300

    Added table dump with mock data.

--- migrations/m170722_075053_url.php
+++ migrations/m170722_075053_url.php
@@ -6,6 +6,7 @@ class m170722_075053_url extends Migration
 {
     public function safeUp()
     {
+        // Create table
         $this->createTable('url', [
             'id' => $this->primaryKey()->notNull(),
             'url' => $this->string(250)->notNull(),
@@ -13,6 +14,9 @@ class m170722_075053_url extends Migration
             'status_code' => $this->integer(3)->defaultValue(0),
             'check_time' => $this->timestamp()->defaultExpression('NOW()'),
         ],'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB');
+
+        // Load mock data
+        $this->execute(file_get_contents('tests/sql/url.sql'));
     }
 
     public function safeDown()
